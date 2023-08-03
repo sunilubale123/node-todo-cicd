3740dcdd09f3f1b9d53e591d0ac76abefa9fdf5e
sunilubale123 <136223844+sunilubale123@users.noreply.github.com>
2023-08-03 16:17:35 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const express = require('express'),
     // Mitigate XSS using sanitizer
     sanitizer = require('sanitizer'),
     app = express(),
+    
     port = 8000
 
 app.use(bodyParser.urlencoded({
